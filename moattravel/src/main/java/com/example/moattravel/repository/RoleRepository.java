2c7335b60853ff759714fb84457f848041911189
oyama <oyama@DEML>
2025-08-08 14:21:21 +0900

    14:00分です

--- moattravel/src/main/java/com/example/moattravel/repository/RoleRepository.java
+++ moattravel/src/main/java/com/example/moattravel/repository/RoleRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.moattravel.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+	public Role findByName(String name);
+
 }
